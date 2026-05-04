972f2d34ac82a80e723f7ec46c55539f5fc16641
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-04 20:24:09 +0600

    premitive data type

---
+++ Mission-1 Be a TypeScript Technocrat/Module-1/src/premitive.ts
@@ -0,0 +1,8 @@
+// string, boolean, number, undefined, null
+// TS : never, unknown, any, void
+
+let userName: string = "Shuvo";
+let userID = 1234;
+
+let isAdmin: boolean = false; //explicit
+let idAdmin = true; //implicit
