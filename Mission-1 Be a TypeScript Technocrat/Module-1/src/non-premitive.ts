cd3343022f26f5b3daff8c8ff3ea60f864499ea9
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-04 21:39:55 +0600

    non-premitive

---
+++ Mission-1 Be a TypeScript Technocrat/Module-1/src/non-premitive.ts
@@ -0,0 +1,12 @@
+let bazarList: string[] = ["rice", "lentil", "oil", "salt"];
+bazarList.push("sugar");
+console.log(bazarList);
+
+let miskArray: (string | number)[] = ["Shuvo", 1234, "Hasan", 5678];
+miskArray.push("Rakibul");
+miskArray.push(9012);
+console.log(miskArray);
+
+// tuple
+let coordinates: [number, number] = [40.7128, 74.006];
+console.log(coordinates);
--- Mission-1 Be a TypeScript Technocrat/Module-1/src/premitive.ts
+++ Mission-1 Be a TypeScript Technocrat/Module-1/src/premitive.ts
@@ -5,4 +5,4 @@ let userName: string = "Shuvo";
 let userID = 1234;
 
 let isAdmin: boolean = false; //explicit
-let idAdmin = true; //implicit
+let idAdmin = true; //implicit`
