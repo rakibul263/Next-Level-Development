3947c3fcd9911b73b287f9d95ae60842fe40673a
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-05 15:34:16 +0600

    rename folder

---
+++ Mission-1 Be a TypeScript Technocrat/Module 1 Explore Basic Types of Typescript/dist/non-premitive.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let bazarList = ["rice", "lentil", "oil", "salt"];
+bazarList.push("sugar");
+console.log(bazarList);
+let miskArray = ["Shuvo", 1234, "Hasan", 5678];
+miskArray.push("Rakibul");
+miskArray.push(9012);
+console.log(miskArray);
+//# sourceMappingURL=non-premitive.js.map
---
+++ Mission-1 Be a TypeScript Technocrat/Module 1 Explore Basic Types of Typescript/dist/premitive.js
@@ -0,0 +1,9 @@
+"use strict";
+// string, boolean, number, undefined, null
+// TS : never, unknown, any, void
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = "Shuvo";
+let userID = 1234;
+let isAdmin = false; //explicit
+let idAdmin = true; //implicit`
+//# sourceMappingURL=premitive.js.map
