114dcb73983ee8e89e81ef21a16d8b99f3e62ad2
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-05 23:50:13 +0600

    Generics added

---
+++ Mission-1 Be a TypeScript Technocrat/Module 2 Explore Advance Types of Typescript1-Python Basic/src/ Generics.ts
@@ -0,0 +1,4 @@
+type GenericArray<T> = Array<T>;
+const friends: GenericArray<string> = ["Bulbul", "Arman", "Naeem"];
+const rollNumber: GenericArray<number> = [123, 5456, 2322];
+const isEligibleList: GenericArray<boolean> = [true, false, true];
