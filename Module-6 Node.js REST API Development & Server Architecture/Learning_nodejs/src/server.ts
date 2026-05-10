a17bbe6c97a7da51e69750e56170f1ec3bf3efa2
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-11 00:18:42 +0600

    added json response

--- Module-6 Node.js REST API Development & Server Architecture/Learning_nodejs/src/server.ts
+++ Module-6 Node.js REST API Development & Server Architecture/Learning_nodejs/src/server.ts
@@ -4,13 +4,18 @@ const server: Server = createServer((req: IncomingMessage, res) => {
   const url = req.url;
   const method = req.method;
 
-  if (url === "/" && method === "GET") {
-    res.end("You are now in home Root.");
-  } else if (url === "/about" && method === "GET") {
-    res.end("You are  now in about Root");
-  } else {
-    res.end("404 ERROR\nPage not found");
-  }
+  // if (url === "/" && method === "GET") {
+  //   res.end("You are now in home Root.");
+  // } else if (url === "/about" && method === "GET") {
+  //   res.end("You are  now in about Root");
+  // } else {
+  //   res.end("404 ERROR\nPage not found");
+  // }
+
+  res.writeHead(200, {
+    "content-type": "application/json",
+  });
+  res.end(JSON.stringify({ success: true, message: "Hello API" }));
 });
 
 server.listen(3000, () => {
