99e0b57c2442aae73ec54e7207d61a67b8463cd3
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-13 23:42:41 +0600

    file transfer

---
+++ Mission-1 Be a TypeScript Technocrat/Module 2 Explore Advance Types of Typescript1-Python Basic/dist/typeAssertion.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let anything;
+anything = 12;
+anything;
+let value = "Daffodil";
+let result = value;
+console.log(result);
+//# sourceMappingURL=typeAssertion.js.map
